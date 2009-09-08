ef1ff9ad91ff47197f929463e135036ee21959d1
Rob Bevan <robbevan@xpt.com>
2009-09-08 15:17:24 +0100

    added custom custom file

--- init.el
+++ init.el
@@ -1,6 +1,10 @@
 (add-to-list 'load-path "~/.emacs.d")
 (add-to-list 'load-path "~/.emacs.d/vendor")
 
+; custom place to save customizations
+(setq custom-file "~/.emacs.d/robbevan/custom.el")
+(load custom-file)
+
 (load "robbevan/theme.el")
 
 (provide 'init)
