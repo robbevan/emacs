71c2b11483662825c707544e66e5e53b144b636f
Rob Bevan <robbevan@xpt.com>
2009-09-08 15:40:27 +0100

    enable TextMate Minor Mode

--- init.el
+++ init.el
@@ -1,10 +1,16 @@
 (add-to-list 'load-path "~/.emacs.d")
 (add-to-list 'load-path "~/.emacs.d/vendor")
+(add-to-list 'load-path "~/.emacs.d/vendor/textmate.el")
 
 ; custom place to save customizations
 (setq custom-file "~/.emacs.d/robbevan/custom.el")
 (load custom-file)
 
+; colour theme
 (load "robbevan/theme.el")
+
+; defunkt's TextMate Minor Mode
+(require 'textmate)
+(textmate-mode)
 
 (provide 'init)
